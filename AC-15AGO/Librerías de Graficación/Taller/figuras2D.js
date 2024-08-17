c2a5ef5acaa280371242dcbb7328ff95a1119750
Andres-FGP <andres.fgonzalez120@gmail.com>
2024-08-17 15:41:56 -0500

    Elegir coordenadas polares
    En un commit previo, había indicado que si se le daba la opción de colocar coordenadas polares, aún no salían las opciones para hacerlo, esto ahora si se puede realizar, ya que se realizó el código correspondiente, tal como se mencionó en el commit, se hizo con un "if" donde si el usuario elige realizar coordenadas cartesianas, no se produce ningún cambio, mas sin embargo si elige hacerlo en coordenadas polares ya aparezcan las respectivas opciones de ángulo y radio
    
    Se añade al código HTML la vinculación con el código JavaScript

--- AC-15AGO/Librerías de Graficación/Taller/figuras2D.js
+++ AC-15AGO/Librerías de Graficación/Taller/figuras2D.js
@@ -13,3 +13,13 @@ const ladosInput = document.getElementById('lados');
 const colorRellenoInput = document.getElementById('colorRelleno');
 const colorBordeInput = document.getElementById('colorBorde');
 const dibujarButton = document.getElementById('dibujar');
+
+coordenadasSelect.addEventListener('change', () => {
+    if (coordenadasSelect.value === 'cartesianas') {
+      cartesianasDiv.style.display = 'block';
+      polaresDiv.style.display = 'none';
+    } else {
+      cartesianasDiv.style.display = 'none';
+      polaresDiv.style.display = 'block';
+    }
+  });
